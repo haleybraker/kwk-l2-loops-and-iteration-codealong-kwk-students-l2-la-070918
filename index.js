c295fa1c682f6e5f39b48be48d2e5af4f24cfd44
haleybraker <haleybraker@gmail.com>
2018-07-12 21:35:59 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const names = ['Ada', 'Brendan', "Ali"]
 
 function printBadges(namesArray) {
  for (let i = 0; i < namesArray.length; i++) {
-console.log("Welcome ",printBadges)
+console.log('Welcome' ${names} ,printBadges)
 }
 }
 
