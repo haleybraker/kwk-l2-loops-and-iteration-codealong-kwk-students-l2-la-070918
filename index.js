6881a46f35f9f5e59580269a59910d58eeae930d
haleybraker <haleybraker@gmail.com>
2018-07-12 21:22:52 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // Code your solutions in this file
 
-var printBadges = (['Ada', 'Brendan', "Ali"]);
+printBadges (['Ada', 'Brendan', "Ali"]);
 
 for (var count = 0; count < 3; count++) {
 console.log(printBadges)
